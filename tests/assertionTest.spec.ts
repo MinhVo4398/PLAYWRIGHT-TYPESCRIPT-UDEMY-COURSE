5dc63cebc33ce035d1a34ab26bcf61c2e9935135
MinhVo <minhvo4398@gmail.com>
2024-02-10 10:10:00 +0700

    present_not present assertion

--- tests/assertionTest.spec.ts
+++ tests/assertionTest.spec.ts
@@ -1,9 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test.only("Visible/Hidden Assertion", async ({ page }) => {
+test("Visible/Hidden Assertion", async ({ page }) => {
   await page.goto("https://www.letskodeit.com/practice");
   await expect(page.locator("#displayed-text")).toBeVisible();
   await page.locator("#hide-textbox").click();
   await expect(page.locator("#displayed-text")).toBeHidden();
+  await page.close();
+});
+
+test.only("Present/ Not Present Assertion", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+  //* Before click, the delete button is not show => so that the count =0
+  await expect(page.locator(".added-manually")).not.toHaveCount(1);
+  await page.locator("//button[normalize-space()='Add Element']").click();
+  //* After click Add element, the delete button will come => so that count = 1
+  await expect(page.locator(".added-manually")).toHaveCount(1);
   await page.close();
 });
