29d2410aa2d551654ca34d16e9b52952d17e1e20
MinhVo <minhvo4398@gmail.com>
2024-02-11 06:21:57 +0700

    dropdown handle

--- tests/checkBoxHandling.spec.ts
+++ tests/checkBoxHandling.spec.ts
@@ -1,1 +1,29 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from "@playwright/test";
+
+test("Checkbox button handling", async ({ page }) => {
+  await page.goto("https://demo.automationtesting.in/Register.html");
+  const circketCheckbox = await page.locator("#checkbox1");
+  const movieCheckbox = await page.locator("#checkbox2");
+  const hockeyCheckbox = await page.locator("#checkbox3");
+
+  // Way 1 Assert
+  expect(circketCheckbox).not.toBeChecked();
+  expect(movieCheckbox).not.toBeChecked();
+  expect(hockeyCheckbox).not.toBeChecked();
+
+  // Way 2 Assert
+  expect(await circketCheckbox.isChecked()).toBeFalsy();
+  expect(await movieCheckbox.isChecked()).toBeFalsy();
+  expect(await hockeyCheckbox.isChecked()).toBeFalsy();
+
+  await circketCheckbox.check();
+  await movieCheckbox.check();
+  await hockeyCheckbox.check();
+
+   expect(circketCheckbox).toBeChecked();
+   expect(movieCheckbox).toBeChecked();
+   expect(hockeyCheckbox.isChecked()).toBeTruthy();
+
+
+  await page.close();
+});
---
+++ tests/dropdownHandling.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test("Single Static Dropdown Handling", async ({ page }) => {
+  await page.goto("https://demo.automationtesting.in/Register.html");
+  await page.selectOption("#Skills", {
+    value: "Android",
+  });
+  await page.pause();
+
+  await page.selectOption("#Skills", {
+    label: "Art Design",
+  });
+
+    await page.pause();
+  await page.selectOption("#Skills", {
+    index: 3,
+  });
+
+    await page.pause();
+  await page.close();
+});
