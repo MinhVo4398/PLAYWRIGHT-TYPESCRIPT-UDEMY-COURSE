b78fdb34d02021342b4f16fd1aea1301c38f89b0
MinhVo <minhvo4398@gmail.com>
2024-02-12 01:25:02 +0700

    frame handle

---
+++ tests/frameHandling.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+test.only("Frame Handling using Page.Frame()", async ({ page }) => {
+  await page.goto("https://ui.vision/demo/webtest/frames/");
+
+  //Find total number of Frames
+  const allFrames = page.frames();
+  const allFramesCount = allFrames.length;
+  console.log("Total Frames Count is " + allFramesCount);
+  const frame1 = await page.frame({ url: /.*frame_1.html.*/ });
+  await frame1?.locator("input[name='mytext1']").fill("Playwright");
+  await page.waitForTimeout(5000);
+  await page.close();
+});
+
+test("Frame Handling Using Page.FrameLocator", async ({ page }) => {
+  await page.goto("https://ui.vision/demo/webtest/frames/");
+  const frame1 = page.frameLocator("frame[src='frame_1.html']");
+  //* After get the frame, navigate to the locator of textbox
+  await frame1?.locator("input[name='mytext1']").fill("Playwright");
+  await page.waitForTimeout(5000);
+  await page.close();
+});
