e4c3476dfffa7470b842d1e822e2d19fbb1aefd6
MinhVo <minhvo4398@gmail.com>
2024-02-14 13:09:05 +0700

    API delete

--- tests/apiTest.spec.ts
+++ tests/apiTest.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 var userId;
 
-test("Get user Details using GET API", async ({ request }) => {
+test.skip("Get user Details using GET API", async ({ request }) => {
   const response = await request.get("https://reqres.in/api/users?page=2");
   var responseJson = await response.json();
   console.log(responseJson);
@@ -12,13 +12,13 @@ test("Get user Details using GET API", async ({ request }) => {
 
 test("Creating User using POST API", async ({ request }) => {
   var user = {
-    name: "morpheus",
-    job: "leader",
+    name: "playwright",
+    job: "udemy",
   };
 
- const response =  await request.post("https://reqres.in/api/users", {
-    data:user,
-    headers: {"ACCEPT": "applications/JSON"}
+  const response = await request.post("https://reqres.in/api/users", {
+    data: user,
+    headers: { ACCEPT: "applications/JSON" },
   });
 
   var responseJson = await response.json();
@@ -26,5 +26,32 @@ test("Creating User using POST API", async ({ request }) => {
   expect(responseJson.name).toBe(`${user.name}`);
   expect(responseJson.job).toBe(`${user.job}`);
   userId = responseJson.id;
+  console.log(userId);
+});
 
+test("Update User using PUT API", async ({ request }) => {
+  var user = {
+    name: "automation",
+    job: "course",
+  };
+
+  const response = await request.put("https://reqres.in/api/users/" + userId, {
+    data: user,
+    headers: { ACCEPT: "applications/JSON" },
+  });
+
+  var responseJson = await response.json();
+  expect(response.status()).toBe(200);
+  expect(responseJson.name).toBe(`${user.name}`);
+  expect(responseJson.job).toBe(`${user.job}`);
+  userId = responseJson.id;
+});
+
+test("Delete User using DELETE API", async ({ request }) => {
+  const response = await request.delete(
+    "https://reqres.in/api/users/" + userId
+  );
+  expect(response.status()).toBe(204);
+  const response2 = await request.get("https://reqres.in/api/users/" + userId);
+  console.log(await response2.json());
 });
